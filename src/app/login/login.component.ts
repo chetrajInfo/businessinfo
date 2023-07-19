3b032f36a353ae9ea795f4ec684ece649017f1f9
myinfo <chetraj0101@gmail.com>
2023-07-19 01:12:26 -0500

    user login accessing from database

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -2,6 +2,9 @@ import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { MatDialog } from '@angular/material/dialog';
 import { ResetComponent } from '../reset/reset.component';
+import { Observable } from 'rxjs/internal/Observable';
+import { Router } from '@angular/router';
+import { UserregisterService } from '../userregister.service';
 
 @Component({
   selector: 'app-login',
@@ -18,7 +21,8 @@ export class LoginComponent implements OnInit {
 
   //this constructor and openResetDialog() is used to show pop up message for reseting user name and password
   //when user forget and dont forget to import (import {MatDialog} form '@angular/material/dialog')
-  constructor(public dialog: MatDialog) {}
+  constructor(public dialog: MatDialog, private router: Router, private userRegisterService: UserregisterService) {}
+
 
   openResetDialog() {
     this.dialog.open(ResetComponent);
@@ -29,43 +33,37 @@ export class LoginComponent implements OnInit {
         username : new FormControl(null, Validators.required),
         password : new FormControl(null, Validators.required),
         business : new FormControl(null, Validators.required),
-        token : new FormControl(null, Validators.required)
+        id: new FormControl(null, Validators.required)
     });
   }
   
 
   onSubmit(): void {
     this.isSubmitted = true;
-    this.credentialsError = this.checkCredentials();  // set the credentials error
-
-    if (this.loginForm.valid && !this.credentialsError) { // check also the credentials
-      console.log(this.loginForm.value);
-      this.loginForm.reset();
-      this.isSubmitted = false;
-    } else {
-      console.log("Submission Fail");
-     
-      this.isSubmitted = true;
-    }
+    this.credentialsError = false;
+    this.checkCredentials().subscribe((isValid) => {
+      if (isValid) {
+        console.log(this.loginForm.value);
+        this.loginForm.reset();
+        this.isSubmitted = false;
+        this.router.navigate(['/dashboard']); // Redirect to the dashboard page
+      } else {
+        console.log('Invalid credentials');
+        this.credentialsError= true;
+        // Display an error message indicating invalid credentials
+      }
+    });
   }
   
-  // checks if the input credentials are wrong
-  checkCredentials() {
-    return !(this.loginForm.get('username').value === 'user1' 
-        && this.loginForm.get('password').value === 'user1'
-        && this.loginForm.get('business').value === 'user1'
-        && this.loginForm.get('token').value === 'user1');
+  checkCredentials(): Observable<boolean> {
+    const username = this.loginForm.get('username')?.value;
+    const password = this.loginForm.get('password')?.value;
+    const business = this.loginForm.get('business')?.value;
+    const id = this.loginForm.get('id')?.value;
+  
+    return this.userRegisterService.checkCredentials(username, password, business, id);
   }
   
   
 
-  // add this helper function to check if all form fields are filled
-  areFieldsEmpty() {
-    const username = this.loginForm.get('username').value;
-    const password = this.loginForm.get('password').value;
-    const business = this.loginForm.get('business').value;
-    const token = this.loginForm.get('token').value;
-
-    return this.isSubmitted && (!username || !password || !business || !token);
-  }
 }
--- src/app/userregister.service.ts
+++ src/app/userregister.service.ts
@@ -9,12 +9,30 @@ export class UserregisterService {
   //[x: string]: any;
   private urlBase = "http://localhost:8080/adduser";
 
-  constructor(private httpClient: HttpClient) {
+  constructor(private httpClient: HttpClient) {}
 
-   }
+
+
 
    createNewUser(newUser:Object): Observable <Object> {
 
        return this.httpClient.post(`${this.urlBase}`, newUser);
    }
+
+
+
+
+
+
+   checkCredentials(username: string, password: string, business: string, id: number): Observable<boolean> {
+    // Modify the URL as per your backend endpoint for checking credentials
+    const url = 'http://localhost:8080/api/checkCredentials';
+  
+    // Create the request payload
+    const credentials = { username, password, business, id };
+  
+    // Send the POST request to the backend API
+    return this.httpClient.post<boolean>(url, credentials);
+  }
+  
 }
